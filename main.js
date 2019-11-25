de1dcb7baa82d748b03cf0b62fe8a3feb3150f65
cabal82 <fabriziogallici@gmail.com>
2019-11-25 10:45:44 +0100

    first query call

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 $(document).ready(function () {
+
+  const apiKey = "6dd01b7265c335fd46cc94907c9fefc1";
+
+  var searchMovie = function(queryStr) {
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/movie",
+      method: "GET",
+      data: {
+        api_key: apiKey,
+        query: queryStr  //comment debug error:
+      },
+      success: function (data) {
+        var results = data.results;
+        console.log(results);
+      },
+      error: function (error) {
+        console.log("error: ", error);
+      }
+    });
+  }
+
+
   console.log('prova');
   $('#search-btn').on('click', function() {
-    console.log('click');
-    var searchStr = $('#search-input').val();
-    console.log(searchStr);
+    // console.log('click');
+    var queryStr = $('#search-input').val();
+    // console.log(queryStr);
+    searchMovie(queryStr);
   })
 
 
