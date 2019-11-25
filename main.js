269c5f8fa9e9be513c1e326583eff3a3022e4b1b
cabal82 <fabriziogallici@gmail.com>
2019-11-25 10:56:33 +0100

    start ev data

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
   const apiKey = "6dd01b7265c335fd46cc94907c9fefc1";
 
-  var searchMovie = function(queryStr) {
+  var searchMovie = function (queryStr) {
     $.ajax({
       url: "https://api.themoviedb.org/3/search/movie",
       method: "GET",
@@ -13,16 +13,32 @@ $(document).ready(function () {
       success: function (data) {
         var results = data.results;
         console.log(results);
+        evMovData(results);
       },
       error: function (error) {
         console.log("error: ", error);
       }
     });
+  };
+
+  var evMovData = function (arrObjMov) {
+    for (var i = 0; i < arrObjMov.length; i++) {
+      var title = arrObjMov[i].title;
+      var orig_title = arrObjMov[i].original_title;
+      var lang = arrObjMov[i].original_language;
+      var vote = arrObjMov[i].vote_average;
+      console.log(title, orig_title, lang, vote);
+    }
+
   }
+
+  var printMoviesTemp = function (objMov) {
+
+  };
 
 
   console.log('prova');
-  $('#search-btn').on('click', function() {
+  $('#search-btn').on('click', function () {
     // console.log('click');
     var queryStr = $('#search-input').val();
     // console.log(queryStr);
