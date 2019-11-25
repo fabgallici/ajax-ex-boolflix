1d2c947d592473f8d99555860891c0463a9bfeb4
cabal82 <fabriziogallici@gmail.com>
2019-11-25 10:35:23 +0100

    input init

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
+  console.log('prova');
+  $('#search-btn').on('click', function() {
+    console.log('click');
+    var searchStr = $('#search-input').val();
+    console.log(searchStr);
+  })
+
+
+
+
+
 
 });
+
+
+
+
+
+// https://api.themoviedb.org/3/search/movie?api_key=6dd01b7265c335fd46cc94907c9fefc1&query=ritorno+al+futuro
